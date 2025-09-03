102b65ebf6a334c22e6ab6b4261810214c131a62
RiqWhatever <132927746+RiqWhatever@users.noreply.github.com>
2025-09-02 22:13:50 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
 --Loader of AvstysHub Script!
 if game.PlaceId == 16281300371 or game.PlaceId == 13772394625 or game.PlaceId == 15234596844 or game.PlaceId == 14732610803 or game.PlaceId == 14915220621 or game.PlaceId == 15264892126 then
-    --loadstring blade ball :happy:
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/RiqWhatever/AvstysHub/refs/heads/main/Scripts/BladeBall.lua'))()
 end
